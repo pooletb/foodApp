ae004565a1f9162b395679b37b55a8904948bb23
pooletb <30785929+pooletb@users.noreply.github.com>
2018-11-09 15:47:24 -0500

    constant update

---
+++ foodApp/public/javascripts/search.js
@@ -0,0 +1,20 @@
+window.onload(ConstantRun());
+document.getElementById("searchController").addEventListener("focus", SearchLoading);
+document.getElementById("searchController").addEventListener("blur", SearchLoading);
+
+function ConstantRun() {
+    var element = document.getElementById("searchBar")
+    element.onkeyup = function(event) {
+      var query = element.value
+    }
+}
+
+function SearchLoading() {
+    var element = document.getElementById("searchController")
+    if(element.classList.contains("is-loading")) {
+        element.classList.remove("is-loading");
+    }
+    else {
+        element.classList.add("is-loading");
+    }
+}
