d11a347339730ccec50ecc98a6f73c72431d781e
pooletb <30785929+pooletb@users.noreply.github.com>
2018-11-10 12:49:46 -0500

    cutting out the middleman

--- foodApp/routes/index.js
+++ foodApp/routes/index.js
@@ -1,11 +1,30 @@
-var express = require('express');
+const process = require('process');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const fetch = require("node-fetch");
-var router = express.Router();
+const router = express.Router();
+const app = express();
+
+app.use(cors())
+
+app.use(bodyParser.json());
+
+app.enable('trust proxy');
+
 const crypto = require('crypto'),
     algorithm = 'aes-256-ctr',
     password = 'Mfk+3P0Ee>';
+const knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host : '127.0.0.1',
+        user : 'root',
+        password : 'noSQLisbetter93',
+        database : 'db'
+    }
+    });
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
     var username = req.cookies.username;
     console.log(username)
@@ -40,6 +59,62 @@ router.get('/authenticate/:ptPass/:ePass/:username', function(req, res) {
     console.log(json)
   });
 
+  //REGISTERING A NEW ACCOUNT
+app.post('/api/1', function(request, response){
+    console.log(request.body);
+    var credentials = request.body;
+    var json = {};
+    
+    console.log(credentials.username, credentials.password);
+    knex('user').insert([{username: credentials.username, pass: credentials.password}])
+    .then(() => {
+      console.log(`Successfully created user`);
+      json.result = 0;
+      response.send(json);
+    })
+    .catch((err) => {
+      console.error(`User already exists`, err);
+      response.send(json);
+    });
+  });
+  
+  //LOG INTO AN EXISTING ACCOUNT
+  app.post('/api/2', function(request, response){
+    console.log(request.body);
+    var credentials = request.body;
+    var json = {};
+    console.log(credentials.username, credentials.password);
+    knex('user').where({
+      username: credentials.username,
+      pass:  credentials.password
+    }).select()
+    .then((result) => {
+      if(result[0] === undefined) {
+        console.error(`Failed login attempt`, err);
+        response.send(json);
+      }
+      else if(result[0] != undefined && result[0] != null) {
+        console.log(`User exists, allow login`);
+        var randomPass = randomPassword(50);
+        var encryptedPass = encrypt(randomPass);
+        json.result = 0;  
+        json.ptPass = randomPass
+        json.ePass = encryptedPass
+        response.send(json);
+      }
+    })
+    .catch((err) => {
+      console.error(`Failed login attempt`, err);
+      response.send(json);
+    });
+  });
+  
+  const PORT = process.env.PORT || 52170;
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+  });
+  
   function encrypt(text){
     var cipher = crypto.createCipher(algorithm,password)
     var crypted = cipher.update(text,'utf8','hex')
@@ -52,6 +127,16 @@ router.get('/authenticate/:ptPass/:ePass/:username', function(req, res) {
     var dec = decipher.update(text,'hex','utf8')
     dec += decipher.final('utf8');
     return dec;
+  }
+  
+  function randomPassword(length) {
+    var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOP1234567890";
+    var pass = "";
+    for (var x = 0; x < length; x++) {
+        var i = Math.floor(Math.random() * chars.length);
+        pass += chars.charAt(i);
+    }
+    return pass;
   }
 
 module.exports = router;
