222f83d5761adc7076b358baa562c47322249fcf
pooletb <30785929+pooletb@users.noreply.github.com>
2018-11-12 01:23:43 -0500

    dsdsd

--- foodApp/routes/index.js
+++ foodApp/routes/index.js
@@ -124,7 +124,7 @@ knex.from('homemade_food')
     for(var i = 0; i < homemade_food_ingredients.length; i++) {
       var index = IndexOf(homemade_food_ingredients[i].food_ID, hmfDBFull)
       hmfDBFull[index].ingredients.push(homemade_food_ingredients[i].ingredient_name)
-      console.log(hmfDBFull[index].ingredients)
+      console.log(hmfDBFull[index])
       hmfDBFull[index].ingredients[hmfDBFull[index].ingredients.length - 1].measurement = homemade_food_ingredients[i].ingredient_name;
     }
     console.log(hmfDBFull);
