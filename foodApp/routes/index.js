308a0b45d386a09f937c6914561d03a34da054d6
pooletb <30785929+pooletb@users.noreply.github.com>
2018-11-11 19:31:04 -0500

    dsdsds

--- foodApp/routes/index.js
+++ foodApp/routes/index.js
@@ -47,9 +47,10 @@ for(var i = 0; i < premade_food.length; i++) {
   knex('premade_food').where('premade_food.food_ID', premade_food[i].food_ID).innerJoin('premade_contains', 'premade_food.food_ID', 'premade_contains.food_ID')
   .then((result) => {
     for(var e = 0; e < result.length; e++) {
-      console.log(premade_food)
       containsAllergens.push(result[e].allergen_name)
-      premade_food[i].containsAllergens = containsAllergens
+      if(containsAllergens.length > 0) {
+        premade_food[i].containsAllergens = containsAllergens
+      }
       console.log(premade_food);
     }
   })
