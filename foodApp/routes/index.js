3d363cc77e2d02f385d4db11d27ae65f94576724
pooletb <30785929+pooletb@users.noreply.github.com>
2018-11-12 01:09:04 -0500

    aasa

--- foodApp/routes/index.js
+++ foodApp/routes/index.js
@@ -76,7 +76,7 @@ knex.from('premade_food').innerJoin('made_by', 'premade_food.food_ID', 'made_by.
   })
   .then((result) => {
     for(var i = 0; i < premade_food_allergens.length; i++) {
-      var index = IndexOf(premade_food_allergens.food_ID,pmfDBFull)
+      var index = IndexOf(premade_food_allergens[i].food_ID,pmfDBFull)
       console.log(premade_food_allergens)
       console.log(pmfDBFull[i])
       console.log(index);
