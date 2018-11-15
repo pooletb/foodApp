d0c039c34014d70ee15a3b71e730e43c33a85fcd
pooletb <30785929+pooletb@users.noreply.github.com>
2018-11-15 00:10:09 -0500

    xx

--- foodApp/routes/index.js
+++ foodApp/routes/index.js
@@ -244,7 +244,7 @@ router.get('/authenticate/:ptPass/:ePass/:username/:eUser', function (req, res) 
 
     p().then(function (result) {
       console.log(result);
-      res.render('home', { params: { user: username, pmDB: this.pmfDBFull, hfDB: this.hmfDBFull, iDB: this.ingredients, cat: this.categories, all: this.allergens, man: this.manufacturers, userSaved: result }, title: 'Home' });
+      res.render('home', { params: { user: user, pmDB: this.pmfDBFull, hfDB: this.hmfDBFull, iDB: this.ingredients, cat: this.categories, all: this.allergens, man: this.manufacturers, userSaved: result }, title: 'Home' });
     })
   }
   else {
