4c318f22ea82792a1d085ee4d0c6a12557dd94f9
pooletb <30785929+pooletb@users.noreply.github.com>
2018-11-14 22:33:42 -0500

    cc

--- foodApp/routes/index.js
+++ foodApp/routes/index.js
@@ -319,7 +319,7 @@ function getInfo(user) {
                 return result
               })
           }
-           premade_likes = await a();
+          a().then(premade_likes);
         // homemade_likes = await b();
         // premade_fav = await c();
         // homemade_fav = await d()
