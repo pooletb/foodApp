adca346ff3b661381a4bb5e795cc389789f6b2a5
pooletb <30785929+pooletb@users.noreply.github.com>
2018-11-15 10:39:28 -0500

    dd

--- foodApp/public/javascripts/dbcontrol.js
+++ foodApp/public/javascripts/dbcontrol.js
@@ -193,10 +193,10 @@ function myFunction(num) {
 
         // Loop through all table rows, and hide those who don't match the search query
         for (i = 0; i < options.length; i++) {
-            if (options.name.toUpperCase().indexOf(filter) > -1) {
+            if (options.innerHTML.toUpperCase().indexOf(filter) > -1) {
                 options[i].style.display = "";
             } else {
-                options[i].name.style.display = "none";
+                options[i].innerHTML.style.display = "none";
             }
         }
     }
